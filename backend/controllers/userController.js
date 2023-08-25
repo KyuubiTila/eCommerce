2c3d88c4cf49b9004f9fb287bb75180c422e2dfa
Akingbade Ayotunde <tylerkatsumoto@gmail.com>
2023-08-25 16:07:08 +0100

    login user: bcrypted password check and jwt token signing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,19 +2,61 @@ const db = require('../models');
 const Users = db.Users;
 const bcrypt = require('bcryptjs');
 
+const { sign } = require('jsonwebtoken');
+
 // CREATE USERS
 const createUsers = async (req, res) => {
   const { name, email, password } = req.body;
-  const passwordHash = await bcrypt.hash(password, 10);
-  const userData = await Users.create({
-    name: name,
-    email: email,
-    password: passwordHash,
-    isAdmin: false,
-  });
-  res.status(200).send(userData);
+
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const userData = await Users.create({
+      name: name,
+      email: email,
+      password: passwordHash,
+      isAdmin: false,
+    });
+    res.status(201).send(userData);
+  } catch (error) {
+    console.error('Error while creating user:', error);
+    res
+      .status(500)
+      .json({ error: 'An error occurred while creating the user' });
+  }
+};
+
+//LOGIN USER
+const login = async (req, res) => {
+  const { name, password } = req.body;
+
+  try {
+    const findUser = await Users.findOne({ where: { name: name } });
+
+    if (!findUser) {
+      return res.status(404).json({ error: 'User not found' });
+    } else {
+      const checkPassword = await bcrypt.compare(password, findUser.password);
+      if (checkPassword) {
+        const accessToken = sign(
+          { name: findUser.name, id: findUser.id },
+          'importantsecrete'
+        );
+        return res.json({
+          token: accessToken,
+          name: findUser.name,
+          id: findUser.id,
+        });
+      } else {
+        return res.status(401).json({ error: 'Incorrect password' });
+      }
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ error: 'An error occurred during login' });
+  }
 };
 
 module.exports = {
   createUsers,
+  login,
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,7 @@
-const { createUsers } = require('../controllers/userController');
+const { createUsers, login } = require('../controllers/userController');
 const userRouter = require('express').Router();
 
 userRouter.post('/register', createUsers);
+userRouter.get('/register', login);
 
 module.exports = userRouter;
