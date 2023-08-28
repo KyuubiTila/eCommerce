8e962b850e2134ecb70317482228b9c752f9aab8
Akingbade Ayotunde <tylerkatsumoto@gmail.com>
2023-08-28 15:04:14 +0100

    dynamic price for individual product

--- frontend/src/components/cards/ProductCard.jsx
+++ frontend/src/components/cards/ProductCard.jsx
@@ -2,18 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const ProductCard = ({ product }) => {
-  const {
-    id,
-    rating,
-    description,
-    price,
-    numReviews,
-    countInStock,
-    category,
-    brand,
-    image,
-    name,
-  } = product;
+  const { id, description, image, name } = product;
   return (
     <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <div className="relative mx-auto w-full">
@@ -44,43 +33,6 @@ export const ProductCard = ({ product }) => {
               >
                 {description}
               </p>
-            </div>
-
-            <div className="grid grid-cols-2 grid-rows-2 gap-4 mt-8">
-              <p className="inline-flex flex-col xl:flex-row xl:items-center text-gray-800">
-                <span className="mt-2 xl:mt-0">Category : {category}</span>
-              </p>
-              <p className="inline-flex flex-col xl:flex-row xl:items-center text-gray-800 justify-end">
-                <span>Brand: {brand}</span>
-              </p>
-              <p className="inline-flex flex-col xl:flex-row xl:items-center text-gray-800">
-                <span className="mt-2 xl:mt-0">Instock : {countInStock}</span>
-              </p>
-              <p className="inline-flex flex-col xl:flex-row xl:items-center text-gray-800 justify-end">
-                <span className="mt-2 xl:mt-0">Price : $ {price}</span>
-              </p>
-            </div>
-            <div className="grid grid-cols-2 mt-8 ml-12 justify-center items-center">
-              <div className="flex items-center justify-end">
-                <div className="flex items-center">
-                  <svg
-                    className="w-4 h-4 text-yellow-300 mr-1"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 22 20"
-                  >
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                  </svg>
-                  <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
-                    {rating}
-                  </p>
-                  <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-                  <p className="text-sm font-bold text-gray-900 dark:text-white">
-                    {numReviews} reviews
-                  </p>
-                </div>
-              </div>
             </div>
           </div>
         </Link>
--- frontend/src/pages/IndividualProduct/IndividualProduct.jsx
+++ frontend/src/pages/IndividualProduct/IndividualProduct.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
 import { useProducts } from '../../stores/productStore';
 
 export const IndividualProduct = () => {
@@ -10,6 +10,12 @@ export const IndividualProduct = () => {
   const singleProduct = useProducts((state) => state.singleProduct);
 
   const { id } = useParams();
+
+  const [inputPrice, setInputPrice] = useState(1);
+
+  const handleInputChange = (event) => {
+    setInputPrice(event.target.value);
+  };
 
   useEffect(() => {
     fetchSingleProduct(id);
@@ -26,6 +32,7 @@ export const IndividualProduct = () => {
     image,
     name,
   } = singleProduct;
+  const newPrice = parseFloat(price) * parseFloat(inputPrice);
   return (
     <div className="m-3 max-w-md mx-auto bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <div className="relative mx-auto w-full">
@@ -53,7 +60,6 @@ export const IndividualProduct = () => {
               {description}
             </p>
           </div>
-
           <div className="grid grid-cols-2 grid-rows-2 gap-4 mt-8">
             <p className="inline-flex flex-col xl:flex-row xl:items-center text-gray-800">
               <span className="mt-2 xl:mt-0">Category : {category}</span>
@@ -67,35 +73,57 @@ export const IndividualProduct = () => {
             <p className="inline-flex flex-col xl:flex-row xl:items-center text-gray-800 justify-end">
               <span className="mt-2 xl:mt-0">Price : ${price}</span>
             </p>
+
+            <div className="flex items-center ">
+              <svg
+                className="w-4 h-4 text-yellow-300 mr-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 22 20"
+              >
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+              </svg>
+              <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
+                {rating}
+              </p>
+              <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+              <p className="text-sm font-bold text-gray-900 dark:text-white">
+                {numReviews} reviews
+              </p>
+            </div>
           </div>
           <div className="grid grid-cols-2 mt-8">
-            <div className="flex items-center">
-              <div className="flex items-center">
-                <svg
-                  className="w-4 h-4 text-yellow-300 mr-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
-                  {rating}
-                </p>
-                <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-                <p className="text-sm font-bold text-gray-900 dark:text-white">
-                  {numReviews} reviews
-                </p>
+            {countInStock === 0 ? (
+              <div className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg">
+                CAN NOT PLACE ORDER
               </div>
-            </div>
+            ) : (
+              <input
+                autoComplete="off"
+                type="number"
+                name="price"
+                placeholder="enter amount"
+                value={inputPrice}
+                onChange={handleInputChange}
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                required
+              />
+            )}
 
             <div className="flex justify-end">
-              <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
-                Add To Cart <i className="ml-1 bi bi-plus-lg"></i>
-              </button>
+              {countInStock === 0 ? (
+                <div className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg">
+                  OUT OF STOCK
+                </div>
+              ) : (
+                <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+                  Add To Cart
+                </button>
+              )}
             </div>
           </div>
+          <div>NEW PRICE: ${newPrice.toFixed(2)} </div>
         </div>
       </div>
     </div>
