1021d3ad11023ccd36a708157200af0583459cb7
Akingbade Ayotunde <tylerkatsumoto@gmail.com>
2023-09-04 15:36:43 +0100

    cleaning the header

--- frontend/src/components/appWrappers/Header.jsx
+++ frontend/src/components/appWrappers/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { useAuth } from '../../stores/auth';
 import { useOrders } from '../../stores/orderStore';
 
@@ -10,6 +10,20 @@ export const Header = () => {
   const loggedIn = useAuth((state) => state.loggedIn);
   const logOut = useAuth((state) => state.logOut);
   const orders = useOrders((state) => state.orders);
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    const dropdown = document.getElementById('dropdown');
+    dropdown.classList.toggle('hidden');
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    const dropdown = document.getElementById('dropdown');
+    dropdown.classList.toggle('hidden');
+    setIsDropdownOpen(false);
+  };
 
   const logout = () => {
     localStorage.removeItem('auth');
@@ -35,13 +49,92 @@ export const Header = () => {
   return (
     <nav className="bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-          Commerce
-        </span>
+        <Link to={'/home'}>
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Commerce
+          </span>
+        </Link>
 
         {loggedIn && (
-          <span className="self-center text-sm font-semibold whitespace-nowrap dark:text-white">
-            Welcome: {user}
+          <span className="self-center text-sm font-semibold whitespace-nowrap dark:text-white relative">
+            <button
+              id="dropdownDefaultButton"
+              onClick={toggleDropdown}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              type="button"
+            >
+              Welcome: {user}
+              <svg
+                className="w-2.5 h-2.5 ml-2.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+            <div
+              id="dropdown"
+              className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute right-0 mt-2"
+            >
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownDefaultButton"
+              >
+                <li>
+                  <Link
+                    to={''}
+                    onClick={closeDropdown}
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+                <li>
+                  <NavLink
+                    onClick={changeToggle}
+                    to={'/createProduct'}
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Create Product
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    onClick={closeDropdown}
+                    to={'/shippingAddress'}
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    ShippingAddress
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    onClick={closeDropdown}
+                    to={'/paymentPage'}
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    PaymentPage
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    onClick={logout}
+                    to={'/home'}
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Logout
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </span>
         )}
 
@@ -86,15 +179,7 @@ export const Header = () => {
                 Home
               </NavLink>
             </li>
-            <li>
-              <NavLink
-                onClick={changeToggle}
-                to={'/createProduct'}
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Create Product
-              </NavLink>
-            </li>
+
             <li>
               <NavLink
                 onClick={changeToggle}
@@ -105,7 +190,7 @@ export const Header = () => {
                 {uniqueOrders.length}
               </NavLink>
             </li>
-            {!loggedIn ? (
+            {!loggedIn && (
               <>
                 <li>
                   <NavLink
@@ -123,36 +208,6 @@ export const Header = () => {
                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     Login
-                  </NavLink>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <NavLink
-                    onClick={logout}
-                    to={'/home'}
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Logout
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    onClick={changeToggle}
-                    to={'/shippingAddress'}
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    ShippingAddress
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    onClick={changeToggle}
-                    to={'/paymentPage'}
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    PaymentPage
                   </NavLink>
                 </li>
               </>
